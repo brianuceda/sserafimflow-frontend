b227efa33e71c7c35bb2c97fd71f603696f658a7
Brian Uceda <monalek22@gmail.com>
2024-10-22 18:53:55 -0500

    fix(dev): guards for testing

--- src/app/core/auth.guards.ts
+++ src/app/core/auth.guards.ts
@@ -2,29 +2,25 @@ import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 
 export const PrivateGuard: CanActivateFn = () => {
-  const router = inject(Router);
+  // const router = inject(Router);
   // const token = localStorage.getItem('token');
-  
+
   // if (!token) {
   //   router.navigateByUrl('/empresa/iniciar-sesion');
   //   return false;
   // }
 
-  router.navigateByUrl('/app/dashboard');
-
   return true;
 };
 
 export const PublicGuard: CanActivateFn = () => {
-  const router = inject(Router);
+  // const router = inject(Router);
   // const token = localStorage.getItem('token');
 
   // if (token) {
   //   router.navigateByUrl('/app/dashboard');
   //   return false;
   // }
-
-  router.navigateByUrl('/app/dashboard');
 
   return true;
 };
