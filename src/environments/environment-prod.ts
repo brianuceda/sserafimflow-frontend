1db23b66a0da5eaef89ad9874cff339f676b7e47
Brian Uceda <89280866+brianuceda@users.noreply.github.com>
2024-10-24 21:39:07 -0500

    test codespaces

--- src/environments/environment-prod.ts
+++ src/environments/environment-prod.ts
@@ -2,5 +2,6 @@ export const environment = {
   PRODUCTION: true,
   ENV_NAME: 'production',
   FRONTEND_URL: 'https://sserafimflow.vercel.app/',
-  BACKEND_URL: 'https://sserafimflow.globitokuki.site/api/v1/',
+  BACKEND_URL: 'https://fuzzy-barnacle-r447jx5rwjf59jq-8080.app.github.dev/api/v1/',
+  // BACKEND_URL: 'https://sserafimflow.globitokuki.site/api/v1/',
 };
