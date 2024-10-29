b79c836a131bdc0b621d3e5d98f10e6e8afd36e0
Brian Uceda <monalek22@gmail.com>
2024-10-29 01:33:15 -0500

    feat(dev): none

--- src/app/features/company/dashboard/dashboard.component.ts
+++ src/app/features/company/dashboard/dashboard.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { ChartOptions } from '../../../shared/data-access/models/chart.model';
+import { ChartApexchartsComponent } from '../../../shared/components/chart-apexcharts/chart-apexcharts.component';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [ChartApexchartsComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export default class DashboardComponent {
+  public chartOptions1!: Partial<ChartOptions>;
 
+  ngOnInit() {
+    this.loadChart1();
+  }
+
+  loadChart1() {
+    this.chartOptions1 = {
+
+    };
+  }
 }
