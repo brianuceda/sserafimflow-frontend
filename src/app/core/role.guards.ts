c702a24c680141ce2fb5d2ff54cb600393b224b0
Brian Uceda <monalek22@gmail.com>
2024-11-15 19:45:37 -0500

    role guards for fast charge on expo

--- src/app/core/role.guards.ts
+++ src/app/core/role.guards.ts
@@ -9,92 +9,129 @@ export interface TokenPayload {
   exp: number;
 }
 
+// export const CompanyGuard: CanActivateFn = () => {
+//   const _router = inject(Router);
+//   const _jwtService = inject(JWTService);
+//   const token = localStorage.getItem('token');
+
+//   console.log('Validando token...');
+
+//   if (!token) {
+//     console.log('Token inválido');
+//     _router.navigateByUrl('/empresa/iniciar-sesion');
+//     return false;
+//   }
+
+//   return new Promise<boolean>((resolve) => {
+//     _jwtService.isValidToken().subscribe((isValid: boolean) => {
+//       if (!isValid) {
+//         console.log('Token inválido');
+//         _jwtService.removeToken();
+//         _jwtService.returnToHome();
+//         resolve(false);
+//         return;
+//       }
+
+//       if (_jwtService.isBank()) {
+//         console.log('Token de Empresa inválido');
+//         _router.navigateByUrl('/app/banco/dashboard');
+//         resolve(false);
+//         return;
+//       }
+
+//       if (!_jwtService.isCompany() && !_jwtService.isBank()) {
+//         console.log('Token inválido');
+//         _jwtService.removeToken();
+//         _jwtService.returnToHome();
+//         resolve(false);
+//         return;
+//       }
+
+//       console.log('Token válido');
+
+//       resolve(true);
+//     });
+//   });
+// };
+
+// export const BankGuard: CanActivateFn = () => {
+//   const _router = inject(Router);
+//   const _jwtService = inject(JWTService);
+//   const token = localStorage.getItem('token');
+
+//   console.log('Validando token...');
+
+//   if (!token) {
+//     _router.navigateByUrl('/banco/iniciar-sesion');
+//     console.log('Token inválido');
+//     return false;
+//   }
+
+//   return new Promise<boolean>((resolve) => {
+//     _jwtService.isValidToken().subscribe((isValid: boolean) => {
+//       if (!isValid) {
+//         console.log('Token inválido');
+//         _jwtService.removeToken();
+//         _jwtService.returnToHome();
+//         resolve(false);
+//         return;
+//       }
+
+//       if (_jwtService.isCompany()) {
+//         console.log('Token de Banco inválido');
+//         _router.navigateByUrl('/app/empresa/dashboard');
+//         resolve(false);
+//         return;
+//       }
+
+//       if (!_jwtService.isCompany() && !_jwtService.isBank()) {
+//         console.log('Token inválido');
+//         _jwtService.removeToken();
+//         _jwtService.returnToHome();
+//         resolve(false);
+//         return;
+//       }
+
+//       console.log('Token válido');
+
+//       resolve(true);
+//     });
+//   });
+// };
+
+// For Fast Testing
 export const CompanyGuard: CanActivateFn = () => {
-  const _router = inject(Router);
-  const _jwtService = inject(JWTService);
+  const router = inject(Router);
+  const jwtService = inject(JWTService);
   const token = localStorage.getItem('token');
 
-  console.log('Validando token...');
-
   if (!token) {
-    console.log('Token inválido');
-    _router.navigateByUrl('/empresa/iniciar-sesion');
+    router.navigateByUrl('/empresa/iniciar-sesion');
+    return false;
+  }
+  
+  if (!jwtService.isCompany()) {
+    router.navigateByUrl('/app/banco/dashboard');
     return false;
   }
 
-  return new Promise<boolean>((resolve) => {
-    _jwtService.isValidToken().subscribe((isValid: boolean) => {
-      if (!isValid) {
-        console.log('Token inválido');
-        _jwtService.removeToken();
-        _jwtService.returnToHome();
-        resolve(false);
-        return;
-      }
-
-      if (_jwtService.isBank()) {
-        console.log('Token de Empresa inválido');
-        _router.navigateByUrl('/app/banco/dashboard');
-        resolve(false);
-        return;
-      }
-
-      if (!_jwtService.isCompany() && !_jwtService.isBank()) {
-        console.log('Token inválido');
-        _jwtService.removeToken();
-        _jwtService.returnToHome();
-        resolve(false);
-        return;
-      }
-
-      console.log('Token válido');
-
-      resolve(true);
-    });
-  });
+  return true;
 };
 
 export const BankGuard: CanActivateFn = () => {
-  const _router = inject(Router);
-  const _jwtService = inject(JWTService);
+  const router = inject(Router);
+  const jwtService = inject(JWTService);
   const token = localStorage.getItem('token');
 
-  console.log('Validando token...');
-
   if (!token) {
-    _router.navigateByUrl('/banco/iniciar-sesion');
-    console.log('Token inválido');
+    router.navigateByUrl('/banco/iniciar-sesion');
     return false;
   }
 
-  return new Promise<boolean>((resolve) => {
-    _jwtService.isValidToken().subscribe((isValid: boolean) => {
-      if (!isValid) {
-        console.log('Token inválido');
-        _jwtService.removeToken();
-        _jwtService.returnToHome();
-        resolve(false);
-        return;
-      }
+  if (!jwtService.isBank()) {
+    router.navigateByUrl('/app/empresa/dashboard');
+    return false;
+  }
 
-      if (_jwtService.isCompany()) {
-        console.log('Token de Banco inválido');
-        _router.navigateByUrl('/app/empresa/dashboard');
-        resolve(false);
-        return;
-      }
-
-      if (!_jwtService.isCompany() && !_jwtService.isBank()) {
-        console.log('Token inválido');
-        _jwtService.removeToken();
-        _jwtService.returnToHome();
-        resolve(false);
-        return;
-      }
-
-      console.log('Token válido');
-
-      resolve(true);
-    });
-  });
+  return true;
 };
