19a66fb667a845ea63c041dd22f20c02a9fc31d8
Brian Uceda <u202114785@upc.edu.pe>
2025-04-23 18:46:42 -0500

    fix(dev): change domain

--- src/environments/environment-prod.ts
+++ src/environments/environment-prod.ts
@@ -2,7 +2,6 @@ export const environment = {
   PRODUCTION: true,
   ENV_NAME: 'production',
   FRONTEND_URL: 'https://sserafimflow.vercel.app/',
-  BACKEND_BASE_URL: 'https://sserafimflow.globitokuki.site/',
-  BACKEND_URL: 'https://sserafimflow.globitokuki.site/api/v1/',
-  // BACKEND_URL: 'https://sserafimflow.globitokuki.site/api/v1/',
+  BACKEND_BASE_URL: 'https://sserafimflow.brianuceda.xyz/',
+  BACKEND_URL: 'https://sserafimflow.brianuceda.xyz/api/v1/',
 };
